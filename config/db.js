5187616036a6a7ed89c8816cdaa36d3b6d71ea4f
Kyaw Gyi <kyawzaw.lwin@mdits.com.mm>
2023-03-07 23:50:01 +0630

    changed back to online db

--- config/db.js
+++ config/db.js
@@ -8,8 +8,8 @@ const config = {
     app: {
       name: 'Cherry-K',
     },
-    db: 'mongodb://127.0.0.1:3221', 
-    //db: 'mongodb+srv://dbuser:P7qBNveg8bVO1d2z@cluster0.85ozwwv.mongodb.net/cherry-k?retryWrites=true&w=majority', 
+    //db: 'mongodb://127.0.0.1:3221', 
+    db: 'mongodb+srv://dbuser:P7qBNveg8bVO1d2z@cluster0.85ozwwv.mongodb.net/cherry-k?retryWrites=true&w=majority', 
     
   },
 
@@ -18,8 +18,8 @@ const config = {
     app: {
       name: 'Cherry-K',
     },
-    db: 'mongodb://127.0.0.1:3221', 
-    //db: 'mongodb+srv://dbuser:P7qBNveg8bVO1d2z@cluster0.85ozwwv.mongodb.net/cherry-k?retryWrites=true&w=majority', 
+    //db: 'mongodb://127.0.0.1:3221', 
+    db: 'mongodb+srv://dbuser:P7qBNveg8bVO1d2z@cluster0.85ozwwv.mongodb.net/cherry-k?retryWrites=true&w=majority', 
   },
 };
 
---
+++ dbIndexes.js
@@ -0,0 +1,23 @@
+const MongoClient = require('mongodb').MongoClient;
+const config = require('./config/db')
+
+// Create a new MongoClient
+const client = new MongoClient(config.db);
+
+// Use connect method to connect to the server
+client.connect(function(err) {
+  console.log("Connected successfully to server");
+
+  const db = client.db('cherry-k');
+
+  // Create indexes
+  db.collection('patients').createIndex({ name: 'text', phone: 'text', email: 'text' }, function(err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Patient Indexes Created Successfully!");
+    }
+
+    client.close();
+  });
+});
