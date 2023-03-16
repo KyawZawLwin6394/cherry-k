b1b4adf5df2443b552846fce196d177160c8daed
Kyaw Zaw Lwin <kyawzawlwin.work.6394@gmail.com>
2023-03-16 14:46:10 +0630

    added expense crud

---
+++ app/controllers/expenseController.js
@@ -0,0 +1,101 @@
+'use strict';
+const Expense = require('../models/expense');
+const Transaction = require('../models/transaction');
+
+exports.listAllExpenses = async (req, res) => {
+    let { keyword, role, limit, skip } = req.query;
+    let count = 0;
+    let page = 0;
+    try {
+        limit = +limit <= 100 ? +limit : 10; //limit
+        skip = +skip || 0;
+        let query = { isDeleted: false },
+            regexKeyword;
+        role ? (query['role'] = role.toUpperCase()) : '';
+        keyword && /\w/.test(keyword)
+            ? (regexKeyword = new RegExp(keyword, 'i'))
+            : '';
+        regexKeyword ? (query['name'] = regexKeyword) : '';
+        let result = await Expense.find(query).limit(limit).skip(skip).populate('initialCurrency').populate('finalCurrency').populate('relatedAccounting');
+        console.log(result)
+        count = await Expense.find(query).count();
+        const division = count / limit;
+        page = Math.ceil(division);
+
+        res.status(200).send({
+            success: true,
+            count: count,
+            _metadata: {
+                current_page: skip / limit + 1,
+                per_page: limit,
+                page_count: page,
+                total_count: count,
+            },
+            list: result,
+        });
+    } catch (e) {
+        return res.status(500).send({ error: true, message: e.message });
+    }
+};
+
+exports.getExpense = async (req, res) => {
+    const result = await Expense.find({ _id: req.params.id, isDeleted: false }).populate('initialCurrency').populate('finalCurrency').populate('relatedAccounting')
+    if (!result)
+        return res.status(500).json({ error: true, message: 'No Record Found' });
+    return res.status(200).send({ success: true, data: result });
+};
+
+exports.createExpense = async (req, res, next) => {
+    try {
+        const newBody = req.body;
+        const newExpense = new Expense(newBody);
+        const result = await newExpense.save();
+        res.status(200).send({
+            message: 'Expense create success',
+            success: true,
+            data: result
+        });
+    } catch (error) {
+        return res.status(500).send({ "error": true, message: error.message })
+    }
+};
+
+exports.updateExpense = async (req, res, next) => {
+    try {
+        const result = await Expense.findOneAndUpdate(
+            { _id: req.body.id },
+            req.body,
+            { new: true },
+        ).populate('relatedAccounting').populate('initialCurrency').populate('finalCurrency');
+        return res.status(200).send({ success: true, data: result });
+    } catch (error) {
+        return res.status(500).send({ "error": true, "message": error.message })
+    }
+};
+
+exports.deleteExpense = async (req, res, next) => {
+    try {
+        const result = await Expense.findOneAndUpdate(
+            { _id: req.params.id },
+            { isDeleted: true },
+            { new: true },
+        );
+        return res.status(200).send({ success: true, data: { isDeleted: result.isDeleted } });
+    } catch (error) {
+        return res.status(500).send({ "error": true, "message": error.message })
+
+    }
+}
+
+exports.activateExpense = async (req, res, next) => {
+    try {
+        const result = await Expense.findOneAndUpdate(
+            { _id: req.params.id },
+            { isDeleted: false },
+            { new: true },
+        );
+        return res.status(200).send({ success: true, data: { isDeleted: result.isDeleted } });
+    } catch (error) {
+        return res.status(500).send({ "error": true, "message": error.message })
+    }
+};
--- app/models/expense.js
+++ app/models/expense.js
@@ -5,52 +5,45 @@ mongoose.promise = global.Promise;
 const Schema = mongoose.Schema;
 
 
-let BankSchema = new Schema({
-  bankName: {
-    type: String,
-    required: true
-  },
-  accountNumber: {
-    type: Number,
-    required: true,
-  },
-  accountHolderName: {
-    type:String,
-    required:true
-  },
-  bankContact: {
-    type: String,
-    required:true,
-  },
-  openingDate: {
-    type: Date,
-    required:true,
-  },
-  balance: {
-    type: Number,
-    required:true,
-  },
-  bankAddress: {
-    type: String,
-    required:true,
-  },
-  isDeleted: {
-    type:Boolean,
-    required:true,
-    default:false
-  },
-  relatedCurrency: {
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Currencies',
-    required:true
-  },
-  relatedAccounting:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'AccountingLists',
-    required:true
-  }
+let ExpenseSchema = new Schema({
+    relatedAccounting: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'AccountingLists',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    remark: {
+        type: String,
+        required: true
+    },
+    initialAmount: {
+        type: Number,
+        required: true,
+    },
+    initialCurrency: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Currencies',
+        required: true,
+    },
+    finalAmount: {
+        type: Number,
+        required: true,
+    },
+    finalCurrency: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Currencies',
+        required: true,
+    },
+    isDeleted: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
 });
 
-module.exports = mongoose.model('Banks', BankSchema);
+module.exports = mongoose.model('Expenses', ExpenseSchema);
 
 //Author: Kyaw Zaw Lwin
