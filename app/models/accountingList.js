820d1c013dd90684845463984d06ddbff0e2d5b8
Kyaw Zaw Lwin <kyawzawlwin.work.6394@gmail.com>
2023-03-21 13:41:10 +0630

    latest

--- app/models/accountingList.js
+++ app/models/accountingList.js
@@ -12,7 +12,7 @@ let AccountingListSchema = new Schema({
     },
     accountingTypes: {
         type: String,
-        enum:['Current Assets','Current Liabilities','Equity','Revenue','COGS','Administration Expenses','Operation Expenses','Staff Expenses','Marketing Expenses','Other Expenses','Other Income','Fixed Assets','Longterm Liabilities'],
+        enum:['Current Assets','Payable','Inventory','Receivable','Current Liabilities','Equity','Revenue','COGS','Administration Expenses','Operation Expenses','Staff Expenses','Marketing Expenses','Other Expenses','Other Income','Fixed Assets','Longterm Liabilities'],
         required: true,
     },
     name: {
