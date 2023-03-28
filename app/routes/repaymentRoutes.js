8b78677af6572a7052c165fbabfb7f659cd7ebbd
maymhuekoko <maymyatmoe211099@gmail.com>
2023-03-28 14:39:28 +0630

    last

--- app/routes/repaymentRoutes.js
+++ app/routes/repaymentRoutes.js
@@ -8,12 +8,12 @@ module.exports = (app) => {
 
     app.route('/api/repayment')
         .post(catchError(repayment.createRepayment))
-        .put(verifyToken,catchError(repayment.updateRepayment))
+        .put(catchError(repayment.updateRepayment))
         
     app.route('/api/repayment/:id')
-        .get(verifyToken ,catchError(repayment.getRepayment))
-        .delete(verifyToken,catchError(repayment.deleteRepayment)) 
-        .post(verifyToken ,catchError(repayment.activateRepayment))
+        .get(catchError(repayment.getRepayment))
+        .delete(catchError(repayment.deleteRepayment)) 
+        .post(catchError(repayment.activateRepayment))
 
     app.route('/api/repayments').get(catchError(repayment.listAllRepayments))
     app.route('/api/repayments/:relatedPateintTreatmentid').get(catchError(repayment.getRelatedPayment))
