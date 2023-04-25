3c7da7b0b1c35ff11a430502ff935d4574f5986e
Kyaw Zaw Lwin <kyawzawlwin.work.6394@gmail.com>
2023-04-25 09:40:20 +0630

    added treatment search

--- app/controllers/treatmentController.js
+++ app/controllers/treatmentController.js
@@ -107,3 +107,13 @@ exports.activateTreatment = async (req, res, next) => {
     return res.status(500).send({ "error": true, "message": error.message })
   }
 };
+
+exports.searchTreatments = async (req, res, next) => {
+  try {
+    const result = await Treatment.find({ $text: { $search: req.body.search } })
+    if (result.length===0) return res.status(404).send({error:true, message:'No Record Found!'})
+    return res.status(200).send({ success: true, data: result })
+  } catch (err) {
+    return res.status(500).send({ error: true, message: err.message })
+  }
+}
--- app/routes/treatmentRoutes.js
+++ app/routes/treatmentRoutes.js
@@ -7,13 +7,14 @@ const verifyToken = require('../lib/verifyToken');
 module.exports = (app) => {
 
     app.route('/api/treatment')
-        .post( catchError(treatment.createTreatment))
+        .post(catchError(treatment.createTreatment))
         .put(catchError(treatment.updateTreatment))
-        
+
     app.route('/api/treatment/:id')
-        .get( catchError(treatment.getTreatment))
-        .delete(catchError(treatment.deleteTreatment)) 
-        .post( catchError(treatment.activateTreatment))
+        .get(catchError(treatment.getTreatment))
+        .delete(catchError(treatment.deleteTreatment))
+        .post(catchError(treatment.activateTreatment))
 
     app.route('/api/treatments').get(catchError(treatment.listAllTreatments))
+    app.route('/api/treatment-search').post(catchError(treatment.searchTreatments))
 };
