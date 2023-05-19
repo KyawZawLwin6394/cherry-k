f1e8f3fdad5ccbbba415f3dbe2f8a5dfe7ffdd2b
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-05-19 14:18:08 +0630

    removed required from cherryk-be

--- app/models/treatment.js
+++ app/models/treatment.js
@@ -64,7 +64,8 @@ let TreatmentSchema = new Schema({
         ref:'FixedAssets'
     },
     quantity:Number,
-    perUsageQTY:Number
+    perUsageQTY:Number,
+    unit:String
   }],
   estimateTotalPrice:{
     type: Number,
