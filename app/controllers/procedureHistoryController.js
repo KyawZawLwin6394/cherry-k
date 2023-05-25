02b85b7c5aeecfe59fed883d241353888dc1deef
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-05-25 10:09:22 +0630

    here

--- app/controllers/procedureHistoryController.js
+++ app/controllers/procedureHistoryController.js
@@ -99,7 +99,7 @@ exports.createProcedureHistory = async (req, res, next) => {
     }
     console.log(data)
     const result = await procedureHistory.create(data);
-    const populate = await procedureHistory.find({ _id: result._id }).populate('').populate('relatedPatient').populate('treatmentPackages.item_id').populate('medicineItems.item_id').populate('pHistory');
+    const populate = await procedureHistory.find({ _id: result._id }).populate('medicineItems.item_id customTreatmentPackages.item_id pHistory relatedAppointment relatedTreatmentSelection')
     res.status(200).send({
       message: 'ProcedureHistory create success',
       success: true,
