f44ea3f024e2d9edcce0c8207bf4679eeb9ab963
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-06-12 13:22:20 +0630

    here

---
+++ app/models/accountBalance.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.promise = global.Promise;
+const Schema = mongoose.Schema;
+const validator = require('validator');
+
+
+let AccountBalance = new Schema({
+    relatedAccounting: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'AccountingLists'
+    },
+    type: {
+        type: String,
+        enum: ['Opening', 'Closing']
+    },
+    amount: {
+        type: Number,
+    },
+    date: {
+        type: Date,
+    },
+    remark: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('AccountBalances', AccountBalance);
+
+//Author: Kyaw Zaw Lwin
