d25835c04668c7143d57cc0ebaa9e0d86f2873a5
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-06-14 14:20:55 +0630

    here

--- app/routes/logRoutes.js
+++ app/routes/logRoutes.js
@@ -7,6 +7,8 @@ const verifyToken = require('../lib/verifyToken');
 module.exports = (app) => {
     app.route('/api/logs').get(verifyToken, catchError(log.listAllLog))
     app.route('/api/logs/filter').get(verifyToken, catchError(log.filterLogs))
-    app.route('/api/logs/usage').post(verifyToken, catchError(log.createUsage))
+    app.route('/api/logs/usage')
+        .post(verifyToken, catchError(log.createUsage))
+    app.route('/api/logs/usage/:id').get(verifyToken, catchError(log.getUsage))
     app.route('/api/logs/usage/related-usage/:id').get(verifyToken, catchError(log.getUsageRecordsByUsageID))
 };
