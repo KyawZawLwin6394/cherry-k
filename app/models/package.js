ad217e4f36b40c603babad528ca86b899d64a69a
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-06-19 16:36:56 +0630

    here

---
+++ app/models/package.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.promise = global.Promise;
+const Schema = mongoose.Schema;
+const validator = require('validator');
+
+
+let Package = new Schema({
+    name: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    relatedTreatments: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Treatments'
+    },
+    totalprice: {
+        type: Number
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    relatedDiscount: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Discounts'
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+        
+    }
+});
+
+module.exports = mongoose.model('Packages', Package);
+
+//Author: Kyaw Zaw Lwin
