57376a8bf4819800937f482d22dce81d9f897957
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-07-06 11:26:37 +0630

    here

--- app/controllers/purchaseController.js
+++ app/controllers/purchaseController.js
@@ -77,21 +77,21 @@ exports.createPurchase = async (req, res, next) => {
         } else if (relatedBranch === undefined) {
             data.medicineItems.map(async function (element, index) {
                 const result = await MedicineItems.findOneAndUpdate(
-                    { relatedMedicineItems: element.item_id },
+                    { _id: element.item_id },
                     { $inc: { currentQuantity: element.qty, totalUnit: element.totalUnit } },
                     { new: true },
                 ).populate('supplierName').populate('medicineItems.item_id').populate('procedureItems.item_id')
             })
             data.procedureItems.map(async function (element, index) {
                 const result = await ProcedureItems.findOneAndUpdate(
-                    { relatedProcedureItems: element.item_id },
+                    { _id: element.item_id },
                     { $inc: { currentQuantity: element.qty, totalUnit: element.totalUnit } },
                     { new: true },
                 ).populate('name').populate('relatedCategory').populate('relatedBrand').populate('relatedSubCategory')
             })
             data.accessoryItems.map(async function (element, index) {
                 const result = await AccessoryItems.findOneAndUpdate(
-                    { relatedAccessoryItems: element.item_id },
+                    { _id: element.item_id },
                     { $inc: { currentQuantity: element.qty, totalUnit: element.totalUnit } },
                     { new: true },
                 ).populate('name').populate('relatedCategory').populate('relatedBrand').populate('relatedSubCategory')
