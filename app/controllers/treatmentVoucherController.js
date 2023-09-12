03d41087f49c0f6c81b30dfaa633777eed55cf34
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-09-12 14:14:00 +0630

    here

--- app/controllers/treatmentVoucherController.js
+++ app/controllers/treatmentVoucherController.js
@@ -421,7 +421,7 @@ exports.getTreatmentVoucherWithTreatmentID = async (req, res) => {
 exports.getTreatmentVoucher = async (req, res) => {
     let query = req.mongoQuery
     if (req.params.id) query._id = req.params.id
-    const result = await TreatmentVoucher.find(query).populate('createdBy relatedTreatment relatedAppointment relatedPatient relatedTreatmentSelection medicineItems.item_id')
+    const result = await TreatmentVoucher.find(query).populate('createdBy relatedTreatment relatedAppointment relatedPatient relatedTreatmentSelection medicineItems.item_id multiTreatment.item_id')
     if (!result)
         return res.status(500).json({ error: true, message: 'No Record Found' });
     return res.status(200).send({ success: true, data: result });
